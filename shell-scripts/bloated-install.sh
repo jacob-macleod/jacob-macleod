3cd411341d326189643d72aab5b2ec87c8d3668e
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2021-04-01 19:42:32 +0100

    Update bloated-install.sh

--- shell-scripts/bloated-install.sh
+++ shell-scripts/bloated-install.sh
@@ -1,5 +1,5 @@
 #A bloated install of all the apps I could possibly need - for debian-based linux distros
-sudo apt-get install git
+sudo apt-get install git -y
 sudo apt update
 
 #Install up-clock:
@@ -8,7 +8,7 @@ sudo dpkg --install up-clock_6.2-0~10~ubuntu20.04.1_all.deb
 sudo apt --fix-broken install
 
 #Vim install
-sudo apt-get install vim
+sudo apt-get install vim -y 
 mkdir .vim
 mkdir .vim/colors
 wget https://github.com/arcticicestudio/nord-vim/raw/develop/colors/nord.vim
@@ -16,7 +16,7 @@ mv nord.vim .vim/colors
 wget https://raw.githubusercontent.com/jacob-macleod/jacob-macleod/master/dot-files/vimrc
 mv vimrc .vimrc
 sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-sudo apt-get install vim-gtk3 --fix-missing
+sudo apt-get install vim-gtk3 --fix-missing -y
 
 #Z.lua install
 sudo apt install lua5.2
@@ -24,31 +24,31 @@ wget https://github.com/skywind3000/z.lua/archive/1.8.7.tar.gz
 sudo tar -xf 1.8.7.tar.gz
 
 #Python3 install
-sudo apt-get install python3
-sudo apt-get install python3-pip
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
 
 #NeoVim install
-sudo apt-get install neovim
+sudo apt-get install neovim -y
 wget https://github.com/jacob-macleod/jacob-macleod/raw/master/dot-files/init.vim
 mv init.vim .config/nvim
 
-sudo apt-get install docker.io
+sudo apt-get install docker.io -y
 
 #VSCode install
 wget https://go.microsoft.com/fwlink/?LinkID=760868
 mv index.html?LinkID=760868 vscode.deb
-sudo apt-get install libxkbfile1
+sudo apt-get install libxkbfile1 -y
 sudo dpkg --install vscode.deb
 
-sudo apt-get install synaptic
+sudo apt-get install synaptic -y
 
 #Install snap and the gui snap manager
-sudo apt-get install snapd
-sudo snap install core
-sudo snap install snap-store
+sudo apt-get install snapd -y
+sudo snap install core -y
+sudo snap install snap-store -y
 
 #JVM install
-sudo apt-get install openjdk-11-jdk
+sudo apt-get install openjdk-11-jdk -y
 
 #Eclipse install
 wget http://ftp.fau.de/eclipse/technology/epp/downloads/release/2020-06/R/eclipse-java-2020-06-R-linux-gtk-x86_64.tar.gz
